66cd99bbf14dcbe32a5e1604e10e55703ca6a61b
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2024-03-17 21:27:44 -0300

    dataEntregaFormatada

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
+
+
+
 async function enviar() {
+
   const produto = document.getElementById("produto").value;
   const tipo = document.getElementById("tipo").value;
   const tecido = document.getElementById("tecido").value;
   const gola = document.getElementById("gola").value;
   const manga = document.getElementById("manga").value;
-  const cliente = document.getElementById("cliente").value;
-  const codigo = document.getElementById("codigo").value;
+  const cliente = String(document.getElementById("cliente").value);
+  const codigo = Number(document.getElementById("codigo").value);
+  const diasParaEntrega = parseInt(document.getElementById("diasEntrega").value, 10);
 
+   // Obter a data atual
+   const dataAtual = new Date();
 
-
-
+   // Calcular a data de entrega
+   const dataEntrega = new Date(dataAtual);
+   dataEntrega.setDate(dataEntrega.getDate() + diasParaEntrega);
+ 
+   // Formatar as datas para exibição
+   const formatoData = { year: 'numeric', month: 'numeric', day: 'numeric' };
+   const dataEmissaoFormatada = dataAtual.toLocaleDateString('pt-BR', formatoData);
+   const dataEntregaFormatada = dataEntrega.toLocaleDateString('pt-BR', formatoData);
 
   var objeto = {
-    produto: produto,
-    tipo: tipo,
-    tecido: tecido,
-    gola: gola,
-    manga: manga,
-    cliente: cliente,
-    codigo: codigo
+    Produto: produto,
+    Tipo: tipo,
+    Tecido: tecido,
+    Gola: gola,
+    Manga: manga,
+    Cliente: cliente,
+    Codigo: codigo,
+    Data_entrada: dataEmissaoFormatada,
+    Data_saida: dataEntregaFormatada,
   };
 
   // Faz a requisição para enviar o objeto para a planilha
   try {
-    const response = await fetch("https://sheetdb.io/api/v1/tlid7sxr7elr2", {
+    const response = await fetch("https://sheetdb.io/api/v1/6lcrabngw0txr", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
